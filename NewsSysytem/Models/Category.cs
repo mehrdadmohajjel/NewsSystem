b8f47447e951105db9891bfa583452dc73cc19c0
Mehrdad Mohajjel <mehrdad.mohajjel@gmail.com>
2022-06-08 07:47:22 +0430

    Initialize *: project first initializing done

---
+++ NewsSysytem/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace NewsSysytem.Models
+{
+    public class Category
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required(ErrorMessage = "Please enter Title.")]
+        public string Title { get; set; }
+        public long DisplayOrder { get; set; }
+        public DateTime CreationDateTime { get; set; } = DateTime.Now;
+    }
+}
