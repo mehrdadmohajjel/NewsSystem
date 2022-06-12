9b4683f1ffe1fe3de1bb5cb3b6d1a27f7aba719a
mehrdadmohajjel <mehrdad.mohajjel@gmail.com>
2022-06-12 11:00:36 +0430

    Feature *: migration added to project

---
+++ NewsSysytem/Models/Topic.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewsSysytem.Models
+{
+    public class Topic
+    {
+        [Key]
+        public long Id { get; set; }
+        public long CaregoryId { get; set; }
+        [Required(ErrorMessage = "Please enter Title.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Please enter Title.")]
+        public string NewsShortBody { get; set; }
+        [Required(ErrorMessage = "Please enter Title.")]
+        public string NewsFullBody { get; set; }
+        public string NewsImage { get; set; }
+        public string NewsTag { get; set; }
+
+        public DateTime CreationDateTime { get; set; } = DateTime.Now;
+
+    }
+}
