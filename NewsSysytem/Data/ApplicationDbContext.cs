1eb780d3f098b24c745792c4fe9d0d027983f490
mehrdadmohajjel <mehrdad.mohajjel@gmail.com>
2022-06-12 08:28:04 +0430

    Feature *: add connection string to project done
    connection string added to project in  program.cs and appsetting,js

--- NewsSysytem/Data/ApplicationDbContext.cs
+++ NewsSysytem/Data/ApplicationDbContext.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NewsSysytem.Models;
 
 namespace NewsSysytem.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+        }
+        public DbSet<Category>Categories { get; set; }       
+
+
     }
 }
